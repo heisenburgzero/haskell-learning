c962dfc628c3d25864da19c1fb21f1c5292acb7f
heisenburgzero <heisenburgzero@gmail.com>
2014-07-18 13:47:13 +0800

    lecture 2 complete

--- CIS194_Intro/lecture2.hs
+++ CIS194_Intro/lecture2.hs
@@ -47,3 +47,28 @@ stan  = Person "Stan" 94 Cabbage
 
 getAge :: Person -> Int
 getAge (Person _ a _) = a
+
+baz :: Person -> String
+baz p@(Person n _ _) = "The name field of (" ++ show p ++ ") is " ++ n
+
+checkFav :: Person -> String
+checkFav (Person n _ SealingWax) = n ++ ", you're my kind of person!"
+checkFav (Person n _ _)          = n ++ ", your favorite thing is lame"
+
+ex03 = case "Hello" of
+           []      -> 3
+           ('H':s) -> length s
+           _       -> 7
+
+data IntList = Empty | Cons Int IntList
+
+intListProd :: IntList -> Int
+intListProd Empty      = 1
+intListProd (Cons x l) = x * intListProd l
+
+data Tree = Leaf Char
+          | Node Tree Int Tree
+  deriving Show
+
+tree :: Tree
+tree = Node (Leaf 'x') 1 (Node (Leaf 'y') 2 (Leaf 'z'))
