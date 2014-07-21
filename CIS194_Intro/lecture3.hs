fb3be638a1b20b8ebe7fa03b83192c8f9dd220f6
heisenburgzero <heisenburgzero@gmail.com>
2014-07-21 18:30:06 +0800

    lecture3 Prelude

--- CIS194_Intro/lecture3.hs
+++ CIS194_Intro/lecture3.hs
@@ -45,3 +45,26 @@ even2 x
     | divBy2reminder x == 0 = True
     | otherwise = False
 
+data List t = E | C t (List t)
+    deriving Show
+
+lst1 :: List Int
+lst1 = C 3 (C 5 (C 2 E))
+
+lst2 :: List Char
+lst2 = C 'x' (C 'y' (C 'z' E))
+
+lst3 :: List Bool
+lst3 = C True (C False E)
+
+filterList :: (t -> Bool) -> List t -> List t
+filterList _ E = E
+filterList p (C x xs)
+      | p x         = C x (filterList p xs)
+      | otherwise   = filterList p xs
+
+mapList :: (t->t) -> List t -> List t
+mapList _ E = E
+mapList p (C x xs) = C (p x) (mapList p xs)
+
+    
